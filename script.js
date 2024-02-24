20f90108f071e858ebc1da0d12a03daf6eabc6a9
JacopoLombardi <lombajacopo@gmail.com>
2024-02-24 14:16:44 +0100

    [add] added Reset button

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const firstnameLastname = document.querySelector('.input_firstname_lastname');
 const km = document.querySelector('.input_km');
 const age = document.querySelector('.input_age');
 const btn_1 = document.querySelector('.btn_1');
+const btn_2 = document.querySelector('.btn_2');
 
 let prezzoFinale = '';
 
@@ -41,15 +42,23 @@ btn_1.addEventListener('click', function(){
    }
    // scrivo il prezzo finale sul ticket
    console.log('prezzoFinale--->', prezzoFinale);
-   document.querySelector('.out_priceticket').innerHTML = `&euro;${prezzoFinale}`;
+   document.querySelector('.out_priceticket').innerHTML = `&euro;${prezzoFinale.toFixed(2)}`;
 
 
    // numero carrozza - da 1 a 10
-   let nCarrozza = Math.floor(Math.random () * 11);
+   const nCarrozza = Math.ceil(Math.random () * 12);
    document.querySelector('.out_carrozza').innerHTML =`${nCarrozza}`;
 
 
    // Codice CP - da 10.000 a 90.000
-   let nCp = Math.floor(Math.random() * (90000 - 10000 + 1)) + 10000;
+   const nCp = Math.floor(Math.random() * (90000 - 10000 + 1)) + 10000;
    document.querySelector('.out_cpcode').innerHTML =`${nCp}`;
+})
+
+
+// se viene premuto btn_2 si resettano i valori
+btn_2.addEventListener('click', function(){
+   document.querySelector('.input_firstname_lastname').value = '';
+   document.querySelector('.input_km').value = '';
+   document.querySelector('.input_age').value = '';
 })
